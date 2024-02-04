8166d590b7134afd82493dff768b455752de96be
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2024-02-04 20:01:12 +0200

    finalcommit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const currentUl = document.getElementsByClassName("gallery")[0];
 
-console.log(galleryItems);
+const CollectionImages = galleryItems.map((galleryItem) =>
+  galleryItem =`
+        <li class="gallery__item">
+        <a class="gallery__link" href=${galleryItem.original}>
+            <img
+            class="gallery__image"
+            src=${galleryItem.preview} 
+            data-source=${galleryItem.original}
+            alt=${galleryItem.description} />
+        </a>
+      </li>`
+)
+currentUl.innerHTML = CollectionImages.join('');
+
+currentUl.addEventListener('click', (e) => {
+  e.preventDefault();  
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  } 
+  // const modalImage = e.target.getAttribute('data-source');
+  const modalImage = e.target.closest('a');
+  console.log(modalImage);
+  const pictureInfo = e.target.alt;
+  const instance = basicLightbox.create(
+    `<h1 style="font-size: 32px; color: red; text-align: center;">Welcome to the Gallery</h1>
+    <h2 style="font-size: 28px; color: yellow; text-align: center;">Pictures name: ${pictureInfo}</h2>
+    <img src="${modalImage}" >`)
+  instance.show()
+
+  currentUl.addEventListener('keydown', e => {
+ if  (e.code === 'Escape') {
+    instance.close()
+  } 
+})
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const currentUl = document.getElementsByClassName("gallery")[0];
 
-console.log(galleryItems);
+const CollectionImages = galleryItems.map((galleryItem) =>
+    galleryItem =`
+        <li class="gallery__item">
+        <a class="gallery__link"  href=${galleryItem.original}>
+            <img
+            class="gallery__image" 
+            src=${galleryItem.preview}
+            alt=${galleryItem.description} />
+        </a>
+      </li>`
+)
+currentUl.innerHTML = CollectionImages.join('');
+
+
+const lightbox = new SimpleLightbox(`.gallery a`,
+    {
+        overlayOpacity: 0.9, captionType: "tetx",
+        captionsData: "alt", heightRatio: 0.9, widthRatio: 0.9,
+        scaleImageToRatio: true, scrollZoom: true,
+        captionPosition: 'bottom', captionDelay: 250,
+        showCounter: false
+    });
